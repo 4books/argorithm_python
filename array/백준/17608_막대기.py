9a704cfcc43a329acaec40032e1bb2a2d30fa491
4books <joonwoo9192@gmail.com>
2023-09-06 20:37:01 +0900

    알고리즘

---
+++ array/백준/17608_막대기.py
@@ -0,0 +1,17 @@
+def solution(sticks):
+    answer = 1
+    src = sticks.pop()
+    for _ in range(len(sticks)):
+        tgt = sticks.pop()
+        if tgt > src:
+            answer += 1
+            src = tgt
+
+    return answer
+
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+print(solution(arr))
